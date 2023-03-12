ad1ddc6a17ad94ec4b46647c13af12766e821f43
ffTsuzuku <gagandeepsandhu@protonmail.com>
2023-03-11 19:09:36 -0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+import { Button } from '@chakra-ui/react'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div className='App'>
+            <Button colorScheme='blue'>Button</Button>
+        </div>
+    )
+}
+
+export default App
