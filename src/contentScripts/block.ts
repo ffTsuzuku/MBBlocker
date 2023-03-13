4d7c5df39f4475f4683a4a183ef07b6a846c6188
ffTsuzuku <gagandeepsandhu@protonmail.com>
2023-03-12 21:28:46 -0400

    Added support for building contentScripts

---
+++ src/contentScripts/block.ts
@@ -0,0 +1,3 @@
+alert('Block Page!!!')
+
+export {}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                main2: 'src/main.tsx',
+                sourceContent: 'src/contentScripts/block.ts',
+            },
+            output: {
+                entryFileNames: 'assets/[name].js',
+                chunkFileNames: 'assets/[name].js',
+                assetFileNames: 'assets/[name][extname]',
+            },
+        },
+    },
 })
