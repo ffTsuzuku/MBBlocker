7c891f0384c441c243be24a95631cc2f0aa58b3c
deep <gagandeepsandhu@protonmail.com>
2023-03-18 18:16:19 -0400

    Basic layout for alternative sites

--- src/components/AlternativeSites.tsx
+++ src/components/AlternativeSites.tsx
@@ -1,10 +1,29 @@
-import { Flex, Box, Image, Text } from '@chakra-ui/react'
+import { Flex, Box, Image, Text, theme } from '@chakra-ui/react'
+
+const randomcColor = () => {
+    const { colors } = theme
+    const colorNames = Object.keys(colors).slice(2)
+
+    const randomColor =
+        colorNames[Math.floor(Math.random() * colorNames.length)]
+
+    //@ts-ignore
+    const shadeNames = Object.keys(colors[randomColor])
+    const randomShade =
+        shadeNames[Math.floor(Math.random() * shadeNames.length)]
+
+    //@ts-ignore
+    return colors[randomColor][randomShade]
+}
 
 const SiteCard = () => {
     const imageUrl =
         'https://cdn.pixabay.com/photo/2023/01/01/21/33/mountain-7690893_960_720.jpg'
 
     const footerText = 'Dummy.com'
+    const color = randomcColor()
+    console.log(color)
+
     return (
         <Box
             h={'250px'}
@@ -14,8 +33,15 @@ const SiteCard = () => {
             borderRadius='lg'
             overflow='hidden'
             position='relative'
+            w={'400px'}
+            bgColor={color}
         >
-            <Image src={imageUrl} alt='Card background' />
+            <Image
+                h={'100%'}
+                w={'100%'}
+                src={imageUrl}
+                alt='Card background'
+            />
             <Text
                 position='absolute'
                 top='50%'
@@ -35,21 +61,31 @@ const SiteCard = () => {
 }
 
 const AlternativeSites = () => {
-    const altSites = ['Test', 'Test', 'Test', 'Test', 'Test', 'Test']
+    const altSites = [
+        'Test',
+        'Test',
+        'Test',
+        'Test',
+        'Test',
+        'Test',
+        'Test',
+    ]
     const siteCardsJsx = altSites.map((site) => <SiteCard />)
 
+    const glowColor = randomcColor()
     return (
         <Flex
             alignItems={'center'}
-            justifyContent={'center'}
             h={'100%'}
-            overflow={'hidden'}
+            justifyContent={'center'}
         >
             <Flex
-                alignItems={'flex-start'}
+                boxShadow={`0 0 5px 5px ${glowColor}, 0 0 10px 10px ${glowColor}, 0 0 10px 15px ${glowColor}, 0 0 50px 20px ${glowColor}`}
                 justifyContent={'flex-start'}
+                alignItems={'flex-start'}
                 flexWrap={'wrap'}
-                w={'1200px'}
+                w={'64%'}
+                borderRadius={'7px'}
             >
                 {siteCardsJsx}
             </Flex>
